5c1170bfc43bd8a65245729ce1b0eaab087b54b9
FeLL1kS <40565990+FeLL1kS@users.noreply.github.com>
2018-06-26 13:05:07 +0400

    Add files via upload

---
+++ Story.pas
@@ -0,0 +1,28 @@
+﻿unit story;
+
+interface
+
+uses types, System.Collections.Generic;
+
+function getChapter(chapterId: integer): Chapter;
+implementation
+
+function getChapter(chapterId: integer): Chapter;
+var
+  chapt:Chapter;
+  pictureId: integer;
+  f: text;
+begin
+  assign(f, chapterId + '.txt');
+  reset(f);
+  
+  while not eoln(f) do
+  begin
+    readln(f, chapt.title);
+  end;
+ 
+  
+  getChapter:=chapt;
+end;
+end.
+
