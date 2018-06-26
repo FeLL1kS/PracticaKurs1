33188974c6845cf69ef61a945b714ff13039b04e
K-Voland <40571473+K-Voland@users.noreply.github.com>
2018-06-26 13:07:59 +0400

    Add files via upload

---
+++ GameState.pas
@@ -0,0 +1,51 @@
+unit GameState;
+
+interface
+uses Types;
+procedure LoadChapter();
+function handleOption(buttonID: integer): Slide;
+function getScore(): integer;
+
+implementation
+
+var
+  score, slideId, state: integer;
+  Chap: Chapter;
+
+///изменение состояния
+procedure setState(st: integer);
+begin
+  state := st;
+end;
+
+///возврат состояния
+function getState(): integer;
+begin
+  getState := state
+end;
+
+ ///инициализация     
+procedure LoadChapter(Chapp: Chapter);
+begin
+  score := 0;
+  slideId := 0;
+  Chap := Chapp;
+end;
+
+ ///смена слайда и посчет очков
+function handleOption(buttonID: integer): Slide;
+begin
+  if (buttonId > Chap.slides[slideId].options.length) then begin
+    score := score + Chap.slides[slideId].options[buttonID];
+    slideId := Chap.slides[slideId].options[buttonID].nextSlide;
+    handleOption := Chap.slides[slideId];
+  end;
+end;
+
+///возврат счета
+function getScore(): integer;
+begin
+  getScore := score;
+end;
+
+end.
