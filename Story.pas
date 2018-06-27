940a6fcbf2cb21e54c6353162610831a08992de1
FeLL1kS <40565990+FeLL1kS@users.noreply.github.com>
2018-06-27 11:37:36 +0400

    Update Story.pas

--- Story.pas
+++ Story.pas
@@ -1,16 +1,16 @@
-﻿unit story;
+unit story;
 
 interface
+uses 
 
-uses types, System.Collections.Generic;
+types, System.Collections.Generic;
 
 function getChapter(chapterId: integer): Chapter;
 implementation
 
-
-
 function getChapter(chapterId: integer): Chapter;
 var
+  
   chapt: Chapter;
   f: text;
   txt, image, opt, kus: string;
@@ -20,26 +20,28 @@ begin
   assign(f, 'data/story/' + chapterId + '.txt');
   reset(f); 
   readln(f, chapt.title);
-  
-  while not eof(f) do 
-  begin
+  i := 0;
+  while not eof(f) do begin
     setLength(chapt.slides, chapt.slides.Length + 1);
-    readln(f, txt);
+    readln(f, chapt.slides[i].text);
     readln(f, image);
     readln(f, opt);
-  end;
-  
-  for i := 1 to length(chapt.slides) do
-    chapt.slides[i].picture := 'data/images/slides/' + image + '.jpg';
-  
-  a := opt.Split(';');
-  
-  for i := 0 to 2 do 
-  begin
-    foreach kus in a do
+    
+    chapt.slides[i].picture := 'data/images/slides' + image + '.jpg';
+    a := txt.Split(';');
+    
+    setLength(chapt.slides[i].options,a.Length);
+    
+    for var j := 0 to a.Length - 1 do begin
       b := kus.split('|');
+      chapt.slides[i].options[j].text := b[0];
+      chapt.slides[i].options[j].nextSlide := b[1];
+      chapt.slides[i].options[j].score := b[2];
+    end;
+    
+    i += 1;
   end;
-  
+
   getChapter := chapt;
 end;
 end.
